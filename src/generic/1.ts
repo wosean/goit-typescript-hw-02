e2c2b10259836679ebcff8a24a33f29e4e210df0
wosean <serhii_v@ukr.net>
2023-11-19 20:59:15 +0200

    generic 1.ts-3.ts

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,7 +3,20 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
+// function getPromise () {
+//   return new Promise((resolve) => {
+//     resolve(['Text', 50]);
+//   });
+// }
+
+// getPromise()
+// .then((data) => {
+//   console.log(data);
+// });
+
+// export {};
+
+function getPromise(): Promise<[string, number]> {
   return new Promise((resolve) => {
     resolve(['Text', 50]);
   });
--- src/generic/2.ts
+++ src/generic/2.ts
@@ -4,20 +4,41 @@
   Функція compare повинна повертати AllType.
 */
 
+// type AllType = {
+//   name: string;
+//   position: number;
+//   color: string;
+//   weight: number
+// }
+
+// function compare (top, bottom): AllType {
+//   return {
+//     name: top.name,
+//     color: top.color,
+//     position: bottom.position,
+//     weight: bottom.weight,
+//   }
+// }
+
+// export {};
+
 type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
+function compare(
+  top: Pick<AllType, 'name' | 'color'>,
+  bottom: Pick<AllType, 'position' | 'weight'>
+): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
